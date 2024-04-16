5baa58e342b3b6f4c01fb743aa034a9249402e4f
lmwale24 <124707787+lmwale24@users.noreply.github.com>
2024-04-16 14:10:20 +0100

    TEMPLATE Ecommerce site created for client

---
+++ src/Components/Navbar/navbar.jsx
@@ -0,0 +1,34 @@
+import React from 'react'
+import {Link} from 'react-router-dom'
+import {ShoppingCart} from 'phosphor-react'
+import "./navbar.css";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import {  faMobileScreenButton } from '@fortawesome/free-solid-svg-icons'
+
+
+export const Navbar = () => {
+  return (
+
+    
+    <div className='navbar'> 
+
+     <div className='links' id='logo'>
+      <Link to= "/"><FontAwesomeIcon icon={faMobileScreenButton} /> KT </Link>
+       </div>
+    <div className='links'>
+    
+      <Link to="/"> Shop </Link>
+      <Link to="/cart"> 
+         <ShoppingCart size={32}/> 
+      </Link>
+
+     
+  
+
+    </div>
+    
+ </div>
+ 
+  )
+};
+
---
+++ src/products.js
@@ -0,0 +1,147 @@
+import product1 from "./Assets/1.png";
+import product2 from "./Assets/2.jpg";
+import product3 from "./Assets/3.jpg";
+import product4 from "./Assets/4.jpg";
+import product5 from "./Assets/5.jpg";
+import product6 from "./Assets/6.jpg";
+import product7 from "./Assets/7.jpg";
+import product8 from "./Assets/8.jpg";
+import product9 from "./Assets/9.jpg";
+import product10 from "./Assets/10.jpg";
+import product11 from "./Assets/11.jpg";
+import product12 from "./Assets/12.jpg";
+import product13 from "./Assets/13.jpg";
+import product14 from "./Assets/14.jpg";
+import product15 from "./Assets/15.jpg";
+import product16 from "./Assets/16.jpg";
+import product17 from "./Assets/17.jpg";
+import product18 from "./Assets/18.jpg";
+
+ export const PRODUCTS =  [
+    {
+        id: 1,
+        productName: "Airpods",
+        price: 399.99,
+        productImage: product1,
+
+    },
+
+        {
+        id: 2,
+        productName: "Dre Beats",
+        price: 349.99,
+        productImage: product2,
+
+    },
+
+        {
+        id: 3,
+        productName: "Iphone 14 Promax",
+        price: 999.99,
+        productImage: product3,
+
+    },
+
+        {
+        id: 4,
+        productName: "Ipad Pro (6th Gen 2022)",
+        price: 1438.00,
+        productImage: product4,
+
+    },
+
+        {
+        id: 5,
+        productName: "Iphone 15 Promax Black",
+        price: 1199.00,
+        productImage: product5,
+
+    },
+
+        {
+        id: 6,
+        productName: "macbook",
+        price: 13000.00,
+        productImage: product6,
+
+    },
+
+        {
+        id: 7,
+        productName: "Audioengine A1 Bluetooth Computer Speakers ",
+        price: 339.00,
+        productImage: product7,
+
+    },
+    {
+        id: 8,
+        productName: "Apple watch",
+        price: 319.00,
+        productImage: product8,
+    },
+
+    {
+        id: 9,
+        productName: "Echo Dot",
+        price: 15.00,
+        productImage: product9,
+    },
+    {
+        id: 10,
+        productName: "MageGee Portable Gaming Keyboard",
+        price: 32.88,
+        productImage: product10,
+    },
+    {
+        id: 11,
+        productName: "Gaming Mechanical Keyboard",
+        price: 36.99,
+        productImage: product11,
+    },
+
+    {
+        id: 12,
+        productName: "Canon EOS R5 ",
+        price: 289.99,
+        productImage: product12,
+    },
+    {
+        id: 13,
+        productName: "Beats Studio Buds True Wireless Noise Cancelling Earphones- Red",
+        price: 159.99,
+        productImage: product13,
+    },
+    {
+        id: 14,
+        productName: "Beats Studio Buds True Wireless Noise Cancelling Earphones- Black",
+        price: 159.99,
+        productImage: product14,
+    },
+    {
+        id: 15,
+        productName: "Logitech MX Master 3S ",
+        price: 99.99,
+        productImage: product15,
+    },
+    {
+        id: 16,
+        productName: "Beats Studio Buds True Wireless Noise Cancelling Earphones - White",
+        price: 159.99,
+        productImage: product16,
+    },
+ 
+    {
+        id: 17,
+        productName: "Samsung Galaxy A54 5G 128GB",
+        price: 169.00,
+        productImage: product17,
+    },
+    {
+        id: 18,
+        productName: "Apple iPhone 15 Pro Max ",
+        price: 1099.99,
+        productImage: product18,
+    }
+];
+
+
